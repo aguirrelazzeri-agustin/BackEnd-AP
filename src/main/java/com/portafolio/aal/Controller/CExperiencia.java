fc5188a61fedd29d287fb7a305b1aef93950bc33
Agustin Aguirre Lazzeri <95191761+Lazzeriagustin@users.noreply.github.com>
2022-07-31 04:24:16 -0300

    app al backend

--- src/main/java/com/portafolio/aal/Controller/CExperiencia.java
+++ src/main/java/com/portafolio/aal/Controller/CExperiencia.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "/experiencia")
+@CrossOrigin(origins = "https://aalapfront.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/portafolio/aal/Controller/PersonaController.java
+++ src/main/java/com/portafolio/aal/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
-@CrossOrigin(origins = "/persona")
+@CrossOrigin(origins = "https://aalapfront.web.app/")
 public class PersonaController {
     
     @Autowired IPersonaService ipersonaService;
